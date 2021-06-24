12f374a43e6ffc0f8742771564d7fd0fc58d8bb1
Marcin <marcin.gier@wp.pl>
2021-06-24 19:56:44 +0200

    donation controller, add bugs and donation counting

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -3,19 +3,29 @@ package pl.coderslab.charity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.InstitutionRepository;
 
 
 @Controller
 public class HomeController {
-private final InstitutionRepository institutionRepository;
-public HomeController(InstitutionRepository institutionRepository){
-    this.institutionRepository = institutionRepository;
-}
+    private final InstitutionRepository institutionRepository;
+    private final DonationRepository donationRepository;
+
+
+    public HomeController(InstitutionRepository institutionRepository, DonationRepository donationRepository) {
+
+        this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
+    }
 
     @RequestMapping("/")
     public String homeAction(Model model) {
-    model.addAttribute("institutions", institutionRepository.findAll());
+        model.addAttribute("institutions", institutionRepository.findAll());
+        model.addAttribute("bugs", donationRepository.sumOfBugs());
+        model.addAttribute("sumOfDonations", donationRepository.findAll());
+
         return "index";
     }
 }
+
--- src/main/java/pl/coderslab/charity/repository/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/repository/DonationRepository.java
@@ -1,7 +1,10 @@
 package pl.coderslab.charity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.coderslab.charity.entity.Donation;
 
 public interface DonationRepository extends JpaRepository<Donation, Double> {
+    @Query("SELECT SUM(donation.quantity) FROM Donation donation")
+    Integer sumOfBugs();
 }
